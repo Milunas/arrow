c6fd08373123b1181aa33abe98eff3f29b337acf
milunas <milunas.lukasz@gmail.com>
2021-10-29 15:44:47 +0200

    init

---
+++ src/main/kotlin/com/example/arrow/ArrowApplication.kt
@@ -0,0 +1,11 @@
+package com.example.arrow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArrowApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArrowApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/arrow/ArrowApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.arrow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArrowApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
